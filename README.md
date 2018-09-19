db92b804f266a03693cf85fa836ff5265590fc23
Maria Gabriela Barrios Cantera <barrios.cantera@gmail.com>
2018-09-20 08:58:14 +1000

    Uploading challenges

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-flexbox-challenge
+# Javascript & CSS Flexbox Challenge #
+
+This is a quick challenge to test your ability to work with Javscript and CSS.
+
+### Setup ###
+
+* clone the repository and open "index.html" in your web browser.
+
+## Challenge 1 ##
+
+* Add a remote data source using the below url: 
+`https://9ss7bxey8k.execute-api.ap-southeast-2.amazonaws.com/default/dummy_service`
+* Replace images with those downloaded from this link.
+
+![Challenge 1](https://s3-ap-southeast-2.amazonaws.com/jeremy.q-83.com/js-challenge_1.png)
+
+## Challenge 2 ##
+
+* Add a second row of pricing tables. These should wrap underneath eachother.
+
+![Challenge 2](https://s3-ap-southeast-2.amazonaws.com/jeremy.q-83.com/js-challenge_2.png)
+
+### Challenge 3 ###
+
+* Create a modal to display the image and text associated with the selected record.
+
+![Challenge 3](https://s3-ap-southeast-2.amazonaws.com/jeremy.q-83.com/js-challenge_3.png)
---
+++ functions.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+  const API_URL = 'https://9ss7bxey8k.execute-api.ap-southeast-2.amazonaws.com/default/dummy_service';
+  const $imgs = $('a > img');
+
+  jQuery.ajax(API_URL, {
+    success: function(response){
+      changeImgs(response);
+      createModals(response);
+    },
+  });
+
+  function changeImgs(response) {
+    const objectsArr = response.Data;
+    for(let i = 0; i < objectsArr.length; i++) {
+      let imgUrl = objectsArr[i].node.frontmatter.cover;
+      if($imgs[i]) {
+        $imgs[i].src = imgUrl;
+      }
+    }
+  }
+
+  function createModals(response) {
+    const objectsArr = response.Data;
+    const $body = document.querySelector('body');
+    for(let i = 0; i < objectsArr.length; i++) {
+      //for every object in the arr create these elements
+      let $modal = document.createElement('div');
+      let $modalImg = document.createElement('img');
+      let $modalText = document.createElement('p');
+      $modal.appendChild($modalImg);
+      $modal.appendChild($modalText);
+      $body.appendChild($modal);
+      //asign id and class to modal
+      $($modal).attr('id', 'modal' + i);
+      $($modal).attr('class', 'modal');
+      //adding text to the 'p'
+      $($modalText).text(objectsArr[i].node.excerpt);
+      //adding img
+      $($modalImg).attr('src', objectsArr[i].node.frontmatter.cover);
+    }
+  }
+
+  $('.cta a').click(function() {
+    $('html,body').animate({
+      scrollTop: $('#tours').offset().top,
+    }, 500);
+    return false;
+  });
+
+  $('#tours li').on('click', function() {
+    $('#location').val($('img', this).attr('alt'));
+  });
+});
